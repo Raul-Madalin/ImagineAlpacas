b9bad2fd807d0c550dd04b5d4e5bfa2e4960a04d
Raul-Madalin <raul05madalin@yahoo.com>
2025-02-02 20:09:30 +0200

    Added filter ml

--- chess_microservices/gateway.py
+++ chess_microservices/gateway.py
@@ -6,6 +6,7 @@ from microservices.recommendation_service import recommendation_blueprint
 from microservices.image_service import image_blueprint
 from microservices.initial_load_service import initial_load_blueprint
 from microservices.filter_rdf_service import filter_rdf_blueprint
+from microservices.filter_ml_service import filter_ml_blueprint
 
 app = Flask(__name__)
 CORS(app)
@@ -14,6 +15,7 @@ CORS(app)
 app.register_blueprint(search_blueprint, url_prefix="/")
 app.register_blueprint(filter_blueprint, url_prefix="/")
 app.register_blueprint(filter_rdf_blueprint, url_prefix="/filter")
+app.register_blueprint(filter_ml_blueprint, url_prefix="/filter")
 app.register_blueprint(recommendation_blueprint, url_prefix="/")
 app.register_blueprint(image_blueprint, url_prefix="/images")
 app.register_blueprint(initial_load_blueprint, url_prefix="/")
